44429868b5a6b26f40c7d8aa911c6c5b5262241b
mahirpatel04 <patel.mahir.h@gmail.com>
2024-05-16 09:43:34 -0700

    adding more templates

---
+++ classes/Display.py
@@ -0,0 +1,2 @@
+class DisplayClient:
+    pass
---
+++ main.py
@@ -0,0 +1,19 @@
+from classes.Calendar import Calendar
+from classes.Month import Month
+from classes.Week import Week
+from classes.Day import Day
+from classes.Event import Event
+from classes.Display import DisplayClient
+
+# STEP 1: Welcome Message
+
+# STEP 2: Main loop:
+    # STEP 3: Display Menu
+    # STEP 4: Do Whatever User Asks
+    # STEP 5: Menu Again
+    
+    # rinse and repeat
+    
+# Upon exit:
+    # STEP 6: Show exit message
+    # STEP 7: Terminate program
