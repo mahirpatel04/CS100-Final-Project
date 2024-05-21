f09318b50228b9bd944f306975e6a3e32595c317
Mansoorkid <shaunmansoor2@gmail.com>
2024-05-20 21:36:15 -0700

    remove function

--- classes/Day.py
+++ classes/Day.py
@@ -4,6 +4,24 @@ class Day:
     def __init__(self, date: date, events: list):
         self.events = []
         self.date = date
-        
+    
+
+    
+    def removeEvent(self):
+        name = input("Enter the exact name of the event you're gonna remove ")
+        for item in self.events:
+            if item == name:
+                self.events.remove(name)
+
     def addEvent():
         pass
+
+
+# test
+#things = ["apple", "banana", "orange"]
+#todo = Day(date, things)
+#print(todo.events)
+#todo.events = ["apple", "banana", "orange"]
+#print(todo.events)
+#todo.removeEvent()
+#print(todo.events)
