ce90536c4fea1b44b6b567d60fff0e9b17694aea
mahirpatel04 <patel.mahir.h@gmail.com>
2024-05-21 14:47:14 -0700

    changing ignore

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-from classes.Calendar import Calendar
-from classes.Day import Day
-from classes.Display import DisplayClient
-from classes.Event import Event
-from classes.Month import Month
-from classes.Week import Week
