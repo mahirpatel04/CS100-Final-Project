84204896e10ae69c15a730fc64da089544d67613
bhuvanak04 <vkoth006@xe-01.cs.ucr.edu>
2024-05-20 23:21:59 -0700

    added in addEvent function

--- classes/Day.py
+++ classes/Day.py
@@ -4,5 +4,18 @@ class Day:
     def __init__(self, date: date):
         self.date = date
         
-    def addEvent():
-        pass
+    def addEvent(self):
+        title = input("Enter the event title: ")
+        start_time = input("Enter the event start time (HH:MM): ")
+        end_time = input("Enter the event end time (HH:MM): ")
+        description = input("Enter a description for the event: ")
+        
+        event = {
+            "title": title,
+            "start_time": start_time,
+            "end_time": end_time,
+            "description": description
+        }
+        
+        self.events.append(event)
+        print("Event added successfully.")
