33dc73bdf0db2d63110e5507e7ad2e17ca483a0d
Mansoorkid <46858459+Mansoorkid@users.noreply.github.com>
2024-05-20 21:37:25 -0700

    Delete classes/Event.py

--- classes/Event.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-class Event:
-    def __init__(self, title: str, startTime: date, endTime: date, ID: int):
-        self.title = title
-        self.startTime = startTime
-        self.endTime = endTime
-        self.ID = ID
