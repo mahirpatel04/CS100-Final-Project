a61b36f07dd507458270ecb44bc18d7392c9cc81
mahirpatel04 <patel.mahir.h@gmail.com>
2024-05-31 22:19:41 -0700

    more changes

--- main.py
+++ main.py
@@ -36,33 +36,3 @@ while menuChoice != "6":
     menuChoice = inputHandler.getMenuChoice(calendar)
 else:
     print("User wants to quit, handle saving progress into txt file here")
-'''
-try:
-    while menuChoice != "Quit":
-        calendar.handleUserChoice(menuChoice, displayer, inputHandler)
-    
-except EOFError:
-    print("Done reading from file. Remove this try/except block for final iteration of product")
-    '''
-# STEP 3: Get Input
-#while input("Enter your choice: ") != "q":
-    
-    #print("do something")
-    
-
-
-
-
-
-
-
-# STEP 2: Main loop:
-    # STEP 3: Display Menu
-    # STEP 4: Do Whatever User Asks
-    # STEP 5: Menu Again
-    
-    # rinse and repeat
-    
-# Upon exit:
-    # STEP 6: Show exit message
-    # STEP 7: Terminate program
